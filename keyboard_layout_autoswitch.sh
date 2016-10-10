74a494d27e43c438c24ed435deeacb1932e81aa2
Per Sandstrom <per.sandstrom@purplescout.se>
2016-10-10 08:42:42 +0200

    Add script to auto-switch keyboard layout

---
+++ keyboard_layout_autoswitch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+while read -r line 
+do
+	WINDOW_ID="$(echo  $line | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
+	NAME=$(xprop -id $WINDOW_ID | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+	CURRENT_KEYBOARD="$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')"
+	if [[ $NAME == *"Google Chrome"* ]] && (("$CURRENT_KEYBOARD" == "us")); then
+		$(setxkbmap -model pc105 -layout "se") 
+	elif (("$CURRENT_KEYBOARD" == "se")); then
+		$(setxkbmap -model pc105 -layout "us") 
+	fi
+done < <(xprop -spy -root _NET_ACTIVE_WINDOW)
