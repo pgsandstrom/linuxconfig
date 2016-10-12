27ac38b574a00fd92409e7ab946cf571f99891c4
Per Sandstrom <per.sandstrom@purplescout.se>
2016-10-12 14:26:44 +0200

    Also use swedish keyboard in firefox

--- keyboard_layout_autoswitch.sh
+++ keyboard_layout_autoswitch.sh
@@ -4,7 +4,7 @@ do
 	WINDOW_ID="$(echo  $line | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
 	NAME=$(xprop -id $WINDOW_ID | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 	CURRENT_KEYBOARD="$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')"
-	if [[ $NAME == *"Google Chrome"* ]] && (("$CURRENT_KEYBOARD" == "us")); then
+	if ([[ $NAME == *"Google Chrome"* ]] || [[ $NAME == *"Firefox"* ]]) && (("$CURRENT_KEYBOARD" == "us")); then
 		$(setxkbmap -model pc105 -layout "se") 
 	elif (("$CURRENT_KEYBOARD" == "se")); then
 		$(setxkbmap -model pc105 -layout "us") 
