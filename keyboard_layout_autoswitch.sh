3ddd67d90d9fc9a43de876dbe3ee34fbdf40e2ff
Per Sandstrom <per.sandstrom@purplescout.se>
2016-10-13 11:27:41 +0200

    Catch weird window IDs that i3wm can produce

--- keyboard_layout_autoswitch.sh
+++ keyboard_layout_autoswitch.sh
@@ -2,6 +2,9 @@
 while read -r line 
 do
 	WINDOW_ID="$(echo  $line | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
+	if (("$WINDOW_ID" == "0x0")); then
+		continue
+	fi
 	NAME=$(xprop -id $WINDOW_ID | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 	CURRENT_KEYBOARD="$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')"
 	if ([[ $NAME == *"Google Chrome"* ]] || [[ $NAME == *"Firefox"* ]]) && (("$CURRENT_KEYBOARD" == "us")); then
