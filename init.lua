e4df9460475cb4f51f178bcfe1729912e4236ae0
Sandström Per <per.sandstrom@consultant.volvofinans.se>
2023-10-11 09:51:22 +0200

    Add surround plugin for nvim

---
+++ init.lua
@@ -0,0 +1,27 @@
+-- bootstrap lazy package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- load plugins with lazy package manager
+require("lazy").setup({
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
+
--- init.vim
+++ init.vim
@@ -48,3 +48,15 @@ nnoremap o :set paste<CR>o<Esc>:set nopaste<CR>a
 " Suddenly matching with % broke, now I need this for it to work: 
 runtime macros/matchit.vim 
 
+
+" only load this plugin if we are not using nvim
+" TODO this is currently disabled since I understand nothing about vim plugins. Maybe abandon vim forever and only use nvim?
+" if !has('nvim')
+"	Plug 'tpope/vim-surround'
+" endif
+
+
+" only load this config file if we are using nvim
+if has('nvim')
+	luafile ~/.config/nvim/init.lua
+endif
